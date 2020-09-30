7c1d0140b421b0e608424c27e2b83396ffadabc2
Danny <buenodanny94@gmail.com>
2020-09-30 14:36:18 -0700

    Updates

---
+++ precache-manifest.3cd2c1d4bc52ca4dafa0a41fb79136cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b244b63fecf6d3d4ae38256efbe7bc9",
+    "url": "/ballers-page/index.html"
+  },
+  {
+    "revision": "c763401baf2d0b59d224",
+    "url": "/ballers-page/static/css/main.6e503292.chunk.css"
+  },
+  {
+    "revision": "84c808d9279c9a6c94d2",
+    "url": "/ballers-page/static/js/2.6fc58b3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ballers-page/static/js/2.6fc58b3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c763401baf2d0b59d224",
+    "url": "/ballers-page/static/js/main.1b084acd.chunk.js"
+  },
+  {
+    "revision": "d7d2117f939ff2f6aec0",
+    "url": "/ballers-page/static/js/runtime-main.db2839a8.js"
+  }
+]);
